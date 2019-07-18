e02f8fda49b16480b2b1782b57c6abe30acde38b
Joël <joelrz.4@hotmail.com>
2019-07-18 16:26:39 +0200

    Proper folder organization

--- Rscript/taskactual.R
+++ Rscript/taskactual.R
@@ -6,51 +6,13 @@ setwd(dirname(current_path))
 setwd("..")
 rm(current_path)
 
-products <- read_csv("datasets/existingproductattributes2017.csv")
-newproducts <- read_csv("datasets/newproductattributes2017.csv")
-set.seed(619)
+products <- read_csv("./Datasets/existingproductattributes2017.csv")
+newproducts <- read_csv("./Datasets/newproductattributes2017.csv")
+set.seed(69)
+
 ####Visualizations####
 
 plotprod <- products[, c(3:11, 13:18)]
-View(plotprod)
-
-for (i in names(plotprod[, -which(names(plotprod) == "Volume")])) {
-  print(ggplot(data = plotprod, 
-               aes_string(x=i, y = plotprod$Volume)) 
-        + geom_jitter(color = "darkred")
-        + ylab("Volume")
-  )
-}
-
-names(plotprod)
-
-sum(is.na(plotprod))
-
-####Data preprocessing####
-#Check duplicated rows
-#Check NA
-#Check outliers
-#Correlation Matrix
-#Check for colinearity
-#Check abnormalities (x5StarsReview had perfect correlation)
-#Normalization, standarization
-####Feature Selection####
-#Create new features (combining)
-##Remove "bad" features (features that are strongly correlated (regression))
-##PCA
-####Modeling####
-##Try different models
-##Try different features
-
-
-
-products <- read_csv("datasets/existingproductattributes2017.csv")
-newproducts <- read_csv("datasets/newproductattributes2017.csv")
-####Visualizations####
-
-plotprod <- products[, c(3:11, 13:18)]
-View(plotprod)
-
 for (i in names(plotprod[, -which(names(plotprod) == "Volume")])) {
   print(ggplot(data = plotprod, 
                aes_string(x=i, y = plotprod$Volume)) 
@@ -243,3 +205,4 @@ newproducts$Volume <- predict(rf_model, newproducts)
 
 ##Try different models
 ##Try different features
+
